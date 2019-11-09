08354358c174f24dddb434a91135f8efcc55657f
Bamdres16 <brianm.bra.mm8@gmail.com>
2019-11-09 15:23:24 -0600

    WebService

---
+++ WebService/tec.core.model/Models/ModelAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tec.core.model.Models
+{
+    class ModelAppContext : DbContext
+    {
+        public ModelAppContext (DbContextOptions <ModelAppContext> options) : base(options)
+        {
+
+        }
+    }
+}
