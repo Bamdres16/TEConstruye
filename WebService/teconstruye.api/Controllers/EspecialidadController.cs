b5379f794447b060c287f6982417449e0f440255
Bamdres16 <brianm.bra.mm8@gmail.com>
2019-11-10 01:15:46 -0600

    Se logra conexion con PostgreSQL

---
+++ WebService/teconstruye.api/Controllers/EspecialidadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using Npgsql;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using teconstruye.api.Models;
+
+namespace teconstruye.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EspecialidadController : ControllerBase
+    {
+
+        readonly NpgsqlConnection conn = new NpgsqlConnection(connectionString: "Server=dbteconstruye.postgres.database.azure.com;Database=TEConstruye;Port=5432;User Id=su@dbteconstruye;Password=T3construye;Ssl Mode=Require;");
+        [HttpGet]
+        public IActionResult GetEspecialidad()
+        {
+
+            var especialidad = conn.Query<Especialidad>(sql: "SELECT * FROM especialidad");
+            
+            return Ok(especialidad);
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostEspecialidad (string nombre)
+        {
+            try
+            {
+                await conn.ExecuteAsync(sql: "INSERT INTO especialidad (nombre) VALUES ('" + nombre + "')");
+                return Ok();
+
+            } catch
+            {   
+                return Ok("Ya existe");
+            }
+            
+        }
+    }
+}
