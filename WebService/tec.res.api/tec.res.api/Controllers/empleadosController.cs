7f970d1a8e2bf4e897e19238a6ee16b2f7bc3547
Bamdres16 <brianm.bra.mm8@gmail.com>
2019-11-17 18:54:23 -0600

    Se añade soporte para obras y empleados

---
+++ WebService/tec.res.api/tec.res.api/Controllers/empleadosController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using tec.res.api.Models;
+
+namespace tec.res.api.Controllers
+{
+    public class empleadosController : ApiController
+    {
+        private TEConstruyeEntities db = new TEConstruyeEntities();
+        // Obtener la lista de todos los empleados
+        // GET: api/empleados
+        public object Getempleado()
+        {
+            return from e in db.empleado
+                   select new { e.nombre, e.apellido1, e.apellido2, e.cedula, e.numero_telefono, e.pago_hora, e.id};
+        }
+        // Para obtener los datos de un empleado especifico
+        // GET: api/empleados/5
+        [ResponseType(typeof(empleado))]
+        public async Task<IHttpActionResult> Getempleado(int id)
+        {
+            empleado empleado = await db.empleado.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(empleado);
+        }
+
+        // Este método permite retonar una respuesta a las peticiones, evitando cualquier problema de CORS
+        public IHttpActionResult Options()
+        {
+            HttpContext.Current.Response.AppendHeader("Allow", "GET,DELETE,PUT,POST,OPTIONS");
+            return Ok();
+        }
+        // Para actualizar los datos de empleado
+        // PUT: api/empleados/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putempleado(int id, empleado empleado)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != empleado.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(empleado).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!empleadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        // Para almacenar un nuevo empleado
+        // POST: api/empleados
+        [ResponseType(typeof(empleado))]
+        public async Task<IHttpActionResult> Postempleado(empleado empleado)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.empleado.Add(empleado);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (cedulaExists(empleado.cedula))
+                {
+                    return Content(HttpStatusCode.Conflict, "Ese empleado ya existe");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = empleado.id }, empleado);
+        }
+        // Para eliminar un empleado en especifico
+        // DELETE: api/empleados/5
+        [ResponseType(typeof(empleado))]
+        public async Task<IHttpActionResult> Deleteempleado(int id)
+        {
+            empleado empleado = await db.empleado.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            db.empleado.Remove(empleado);
+            await db.SaveChangesAsync();
+
+            return Ok(empleado);
+        }
+        // Métodos autogenerados
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool empleadoExists(int id)
+        {
+            return db.empleado.Count(e => e.id == id) > 0;
+        }
+        private bool cedulaExists (string cedula)
+        {
+            return db.empleado.Count(e => e.cedula == cedula) > 0;
+        }
+    }
+}
---
+++ WebService/tec.res.api/tec.res.api/Controllers/obrasController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using tec.res.api.Models;
+
+namespace tec.res.api.Controllers
+{
+    public class obrasController : ApiController
+    {
+        private TEConstruyeEntities db = new TEConstruyeEntities();
+        // Obtiene la lista de todas las obras existentes
+        // GET: api/obras
+        public object Getobra()
+        {
+            return from o in db.obra
+                   select new
+                   {
+                       o.area_construccion,
+                       o.area_lote,
+                       o.cantidad_banos,
+                       o.cantidad_habitaciones,
+                       o.cantidad_pisos,
+                       o.id,
+                       o.nombre_obra,
+                       o.propietario,
+                       o.ubicacion
+                   };
+        }
+        // Obtiene una obra en específico
+        // GET: api/obras/5
+        [ResponseType(typeof(obra))]
+        public async Task<IHttpActionResult> Getobra(int id)
+        {
+            obra obra = await db.obra.FindAsync(id);
+            if (obra == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(obra);
+        }
+        // Este método permite retonar una respuesta a las peticiones, evitando cualquier problema de CORS
+        public IHttpActionResult Options()
+        {
+            HttpContext.Current.Response.AppendHeader("Allow", "GET,DELETE,PUT,POST,OPTIONS");
+            return Ok();
+        }
+        // Actualiza los datos de una obra
+        // PUT: api/obras/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putobra(int id, obra obra)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != obra.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(obra).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!obraExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        // Almacena los datos de una obra
+        // POST: api/obras
+        [ResponseType(typeof(obra))]
+        public async Task<IHttpActionResult> Postobra(obra obra)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.obra.Add(obra);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = obra.id }, obra);
+        }
+        // Eliminar una obra por su identificador
+        // DELETE: api/obras/5
+        [ResponseType(typeof(obra))]
+        public async Task<IHttpActionResult> Deleteobra(int id)
+        {
+            obra obra = await db.obra.FindAsync(id);
+            if (obra == null)
+            {
+                return NotFound();
+            }
+
+            db.obra.Remove(obra);
+            await db.SaveChangesAsync();
+
+            return Ok(obra);
+        }
+        // Métodos autogenerados
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool obraExists(int id)
+        {
+            return db.obra.Count(e => e.id == id) > 0;
+        }
+    }
+}
